95ef8a837dbd91b489a3316fa58d81fe63f88007
AntaresKS <101465248+AntaresKS@users.noreply.github.com>
2024-10-31 20:11:22 +0100

    Ajout système de routage :tada:

---
+++ Router/allRoutes.js
@@ -0,0 +1,11 @@
+import Route from "./Route.js";
+
+// Definir ici les routes
+export const allRoutes = [
+  new Route("/", "Accueil", "/pages/home.html"),
+  // new Route("/", "Galerie", "/pages/galerie.html", "js/galerie.js"),
+
+];
+
+  // le titre s'affiche comme ceci : Route.titre - websitename
+export const websitename = "Quai Antique";
